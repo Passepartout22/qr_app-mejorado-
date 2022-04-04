eff66b596a1712e4d3684300d083105868228aa9
zeus <emmanuelcach6@gmail.com>
2022-04-03 19:04:49 -0500

    patichapoy

---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:qr_app/providers/db_provider.dart';
+
+
+class ScanListProvider extends ChangeNotifier {
+
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  Future<ScanModel> nuevoScan( String valor ) async {
+
+    final nuevoScan = new ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+    // Asignar el ID de la base de datos al modelo
+    nuevoScan.id = id;
+
+    if ( this.tipoSeleccionado == nuevoScan.tipo ) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+
+    return nuevoScan;
+  }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getTodosScan();
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  cargarScanPorTipo( String tipo ) async {
+    final scans = await DBProvider.db.getScanPorTipo(tipo);
+    this.scans = [...scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    await DBProvider.db.deleteAllScan();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  borrarScanPorId( id ) async {
+    await DBProvider.db.deleteScan(id);
+  }
+
+
+
+}
+
